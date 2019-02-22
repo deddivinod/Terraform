6344266432432e48d985c91e9f4619d6c00a5836
Paul Paginton <Paul.Paginton@ultima.com>
2019-02-22 10:50:30 +0000

    tidying up indentation

--- Project1/prodvmvariables.tf
+++ Project1/prodvmvariables.tf
@@ -1,50 +1,49 @@
 variable "prodsqlresourcegroupname" {
     default = "WUS-RG-Prod-SQL"
  }
- variable "prodvmlocation" {
+variable "prodvmlocation" {
      default = "West US"
-  }
-
-  variable "prodwebresourcegroupname" {
+ }
+variable "prodwebresourcegroupname" {
       default = "WUS-RG-WEb-Prod"
-   }
-   variable "webvmname" {
+ }
+variable "webvmname" {
        default = "WUSVFAWebprod01"
-    }
-    variable "sqlvmname" {
-        default = "WUSVFASQlProd01"
-     }
-    variable "webvmsize" {
-        default = "Standard_B2ms"
-     }
-     variable "sqlvmsize" {
-         default = "Standard_DS3_v2"
-      }
-     variable "vmpublisher" {
-         default = "MicrosoftWindowsServer"
-      }
-      variable "vmoffer" {
-          default = "WindowsServer"
-       }
-       variable "webvmsku" {
-           default = "2016-DataCenter"
-        }
-        variable "sqlvmsku" {
-            default = "Enterprise"
-         }
-         variable "sqlvmpublisher" { 
-             default = "MicrosoftSQLServer"
-         }
-         variable "sqlvmoffer" {
-             default = "SQL2016SP2-WS2016"
-          }
-        variable "osmanageddisktype" {
-            default = "Premium_LRS"
-         }
+ }
+variable "sqlvmname" {
+       default = "WUSVFASQlProd01"
+ }
+variable "webvmsize" {
+       default = "Standard_B2ms"
+ }
+variable "sqlvmsize" {
+       default = "Standard_DS3_v2"
+ }
+variable "vmpublisher" {
+        default = "MicrosoftWindowsServer"
+ }
+variable "vmoffer" {
+        default = "WindowsServer"
+ }
+variable "webvmsku" {
+        default = "2016-DataCenter"
+ }
+variable "sqlvmsku" {
+        default = "Enterprise"
+ }
+variable "sqlvmpublisher" { 
+         default = "MicrosoftSQLServer"
+ }
+variable "sqlvmoffer" {
+         default = "SQL2016SP2-WS2016"
+ }
+variable "osmanageddisktype" {
+         default = "Premium_LRS"
+ }
 
  
-   variable "prodvmtags" {
+variable "prodvmtags" {
         default     = {
             environment     = "Prod"
-    }
+ }
 }
