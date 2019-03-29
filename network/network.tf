bd51ee07dbc997b9f8784c5b1e1a756f3812eed6
Paul Paginton <pagyP@users.noreply.github.com>
2019-03-29 16:03:33 +0000

    added additioanl subnet

--- network/network.tf
+++ network/network.tf
@@ -9,8 +9,8 @@ module "network" {
   location            = "${azurerm_resource_group.RG_NetworkTF.location}"
   vnet_name           = "TFVnet"
   address_space       = "192.168.0.0/24"
-  subnet_prefixes     = ["192.168.0.0/27", "192.168.0.32/27", "192.168.0.64/27"]
-  subnet_names        = ["GateWaySubnet", "dev", "training"]
+  subnet_prefixes     = ["192.168.0.0/27", "192.168.0.32/27", "192.168.0.64/27","192.168.0.96/27"]
+  subnet_names        = ["GateWaySubnet", "dev", "training","vms"]
   dns_servers         = ["1.1.1.1"]  
     tags              = "${azurerm_resource_group.RG_NetworkTF.tags}"  
 }
