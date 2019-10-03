9e7c9212481338e489ff828d2555bc7835231bbb
Paul Paginton <pagyP@users.noreply.github.com>
2019-10-03 14:39:10 +0100

    service princ vars

--- 0.12/Lab-Infrastructure/mainvariables.tf
+++ 0.12/Lab-Infrastructure/mainvariables.tf
@@ -32,13 +32,13 @@ variable "keyvaultname" {
 variable "tenantid" {
 }
 
-#variable "subscription_id" {}
+variable "subscription_id" {}
 
-#variable "client_id"{}
+variable "client_id"{}
 
-#variable "client_secret"{}
+variable "client_secret"{}
 
-#variable "tenant_id"{}
+variable "tenant_id"{}
 
 
 #variable "devlocaladminpwd" {
