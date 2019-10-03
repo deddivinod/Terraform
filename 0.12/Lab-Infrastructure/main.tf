132c3d91a60776ffdc4854a4f969f0349517487e
Paul Paginton <pagyP@users.noreply.github.com>
2019-10-03 11:54:58 +0100

    updated vars

--- 0.12/Lab-Infrastructure/main.tf
+++ 0.12/Lab-Infrastructure/main.tf
@@ -7,10 +7,10 @@ provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
   version = "=1.34.0"
 
-  subscription_id = "TF_VAR_subscription_id"
-  client_id       = "TF_VAR_client_id"
-  client_secret   = "TF_VAR_client_secret"
-  tenant_id       = "TF_VAR_tenant_id"
+  subscription_id = "TF_VAR_SUBSCRIPTION_ID"
+  client_id       = "TF_VAR_CLIENT_ID"
+  client_secret   = "TF_VAR_CLIENT_SECRET"
+  tenant_id       = "TF_VAR_TENANT_ID"
 }
 resource "azurerm_resource_group" "networkrg" {
   name     = var.networkresourcegroupname
