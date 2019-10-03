d48400c5cf39367a4efb7d48adb7fb066253361e
Paul Paginton <pagyP@users.noreply.github.com>
2019-10-03 14:38:18 +0100

    interpolation for service princ vars

--- 0.12/Lab-Infrastructure/main.tf
+++ 0.12/Lab-Infrastructure/main.tf
@@ -7,10 +7,14 @@ provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
   version = "=1.34.0"
 
-  subscription_id = "TF_VAR_SUBSCRIPTION_ID"
-  client_id       = "TF_VAR_CLIENT_ID"
-  client_secret   = "TF_VAR_CLIENT_SECRET"
-  tenant_id       = "TF_VAR_TENANT_ID"
+  #subscription_id = "TF_VAR_SUBSCRIPTION_ID"
+  #client_id       = "TF_VAR_CLIENT_ID"
+  #client_secret   = "TF_VAR_CLIENT_SECRET"
+  #tenant_id       = "TF_VAR_TENANT_ID"
+  subscription_id =  "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
 }
 resource "azurerm_resource_group" "networkrg" {
   name     = var.networkresourcegroupname
