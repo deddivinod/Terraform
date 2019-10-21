f374625ea7b1980f0318485608ffe76867d47ce9
Paul Paginton <pagyP@users.noreply.github.com>
2019-10-21 09:13:50 +0100

    log analytics updates

--- OMS/LAVariables.tf
+++ OMS/LAVariables.tf
@@ -4,15 +4,21 @@ variable "prefix" {
 }
 
 variable "RGName"{
-  default = "RG_LogAnalyticsTest"
+  default = "RG_LogAnalytics"
 }
 
 variable "location" {
   description = "The location/region where the core network will be created. The full list of Azure regions can be found at https://azure.microsoft.com/regions"
-  default     = "WestEurope"
+  default     = "NorthEurope"
 }
 
 variable "omsname" {
   description = "The name of the OMS workspace"
-  default     = "pmp1"
+  default     = "pmplab"
 }
+ variable "tags" {
+        default     = {
+            source  = "pmp"
+            env     = "lab"
+    }
+         }
--- OMS/LogAnalytics.tf
+++ OMS/LogAnalytics.tf
@@ -1,6 +1,7 @@
 resource "azurerm_resource_group" "loganalyticsRG" {
   name     = "${var.RGName}"
   location = "${var.location}"
+  tags     = "${var.tags}"
 }
 
 resource "azurerm_log_analytics_workspace" "LAWorkspace" {
@@ -9,11 +10,13 @@ resource "azurerm_log_analytics_workspace" "LAWorkspace" {
   resource_group_name = "${azurerm_resource_group.loganalyticsRG.name}"
   sku                 = "PerGB2018"
   retention_in_days   = 30
+  tags =  "${azurerm_resource_group.loganalyticsRG.tags}"
 }
 resource "azurerm_automation_account" "autoaccount" {
   name                = "testautoaccount-01"
   location            = "${azurerm_resource_group.loganalyticsRG.location}"
   resource_group_name = "${azurerm_resource_group.loganalyticsRG.name}"
+  tags =  "${azurerm_resource_group.loganalyticsRG.tags}"
 
   sku {
     name = "Basic"
@@ -25,7 +28,7 @@ resource "azurerm_log_analytics_linked_service" "link" {
   resource_id         = "${azurerm_automation_account.autoaccount.id}"
 }
 
-resource "azurerm_log_analytics_solution" "updates" {
+/*resource "azurerm_log_analytics_solution" "updates" {
   solution_name         = "Updates"
   location              = "${azurerm_resource_group.loganalyticsRG.location}"
   resource_group_name   = "${azurerm_resource_group.loganalyticsRG.name}"
@@ -36,4 +39,5 @@ resource "azurerm_log_analytics_solution" "updates" {
     publisher = "Microsoft"
     product   = "OMSGallery/Updates"
   }
-}
+  }
+  */
